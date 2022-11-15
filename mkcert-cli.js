5220cf42881e34305b193673c123affa936eaae5
Tobias Lundin <tobias.lundin@gmail.com>
2022-11-16 00:01:25 +0100

    do what rome says

--- mkcert-cli.js
+++ mkcert-cli.js
@@ -6,7 +6,7 @@ import { existsSync } from "fs";
 import { mkdir, writeFile } from "fs/promises";
 import minimist from "minimist";
 import pc from "picocolors";
-import mkcert from 'vite-plugin-mkcert';
+import mkcert from "vite-plugin-mkcert";
 
 /**
  * Init, read variables and create folders
@@ -16,9 +16,13 @@ const argv = minimist(process.argv.slice(2));
 const cwd = process.cwd();
 
 const outDir = argv.outDir ? join(cwd, argv.outDir) : defaultOutDir;
-const hosts = argv.host ? (Array.isArray(argv.host) ? argv.host : [argv.host]) : [];
-const certFilePath = join(outDir, argv.cert ?? 'dev.cert');
-const keyFilePath = join(outDir, argv.key ?? 'dev.key');
+const hosts = argv.host
+	? Array.isArray(argv.host)
+		? argv.host
+		: [argv.host]
+	: [];
+const certFilePath = join(outDir, argv.cert ?? "dev.cert");
+const keyFilePath = join(outDir, argv.key ?? "dev.key");
 
 console.log(`
 Running ${pc.green(`${pc.bold("mkcert-cli")}`)}
@@ -37,24 +41,29 @@ if (!existsSync(outDir)) {
 /**
  * Create certificates
  */
-if (!existsSync(certFilePath) && !existsSync(keyFilePath)) {
-  try {
-    /** @type { any } */
-    const { config: installMkCert } = mkcert();
-    const { server: { https: { key, cert } } } = await installMkCert({});
-    console.log('Writing cert files...')
-    await writeFile(keyFilePath, key, { encoding: 'utf-8' });
-    await writeFile(certFilePath, cert, { encoding: 'utf-8' });
-  } catch (writeErr) {
-    console.error(writeErr.toString());
-    process.exit(1);
-  }
-
-  console.log(`🎉 Created "${pc.magenta("dev.cert")}" and "${pc.magenta("dev.key")}"
-  in ${pc.yellow(outDir)}`);
-} else {
-  console.log(`🎉 Files "${pc.magenta("dev.cert")}" and "${pc.magenta("dev.key")}" already exist
+if (existsSync(certFilePath) && existsSync(keyFilePath)) {
+  console.log(`🎉 Files "${pc.magenta("dev.cert")}" and "${pc.magenta(
+		"dev.key",
+	)}" already exist
   in ${pc.yellow(outDir)}`);
 }
+else {
+	try {
+		/** @type { any } */
+		const { config: installMkCert } = mkcert();
+		const {
+			server: { https: { key, cert } },
+		} = await installMkCert({});
+		console.log("Writing cert files...");
+		await writeFile(keyFilePath, key, { encoding: "utf-8" });
+		await writeFile(certFilePath, cert, { encoding: "utf-8" });
+	} catch (writeErr) {
+		console.error(writeErr.toString());
+		process.exit(1);
+	}
 
-
+	console.log(`🎉 Created "${pc.magenta("dev.cert")}" and "${pc.magenta(
+		"dev.key",
+	)}"
+  in ${pc.yellow(outDir)}`);
+}
