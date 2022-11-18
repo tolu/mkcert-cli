3d02bdc908e1ac134d8cfa9348589859225fa873
Tobias Lundin <tobias.lundin@gmail.com>
2022-11-18 18:19:39 +0100

    fix reading version from package.json did only work when cwd was specific folder Fixes #8

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,15 @@
-import { homedir, networkInterfaces } from "os";
+import { homedir, networkInterfaces } from 'os';
 
-import { exec as cp_exec } from "child_process";
-import { promisify } from "util";
-import { dirname, join, resolve } from "path";
-import { existsSync, readFileSync } from "fs";
-import { chmod, mkdir, writeFile as fsWriteFile } from "fs/promises";
+import { exec as cp_exec } from 'child_process';
+import { promisify } from 'util';
+import { dirname, join, resolve } from 'path';
+import { existsSync, readFileSync } from 'fs';
+import { chmod, mkdir, writeFile as fsWriteFile } from 'fs/promises';
 
-export { existsSync } from "fs";
-export { readFile } from "fs/promises";
+export { existsSync } from 'fs';
+export { readFile } from 'fs/promises';
 
-import semver from "semver-compare";
+import semver from 'semver-compare';
 
 const getLocalV4Ips = () => {
   const interfaceDict = networkInterfaces();
@@ -20,7 +20,7 @@ const getLocalV4Ips = () => {
       for (const item of interfaces) {
         /** @type {*} */
         const family = item.family;
-        if (family === "IPv4" || family === 4) {
+        if (family === 'IPv4' || family === 4) {
           addresses.push(item.address);
         }
       }
@@ -31,7 +31,7 @@ const getLocalV4Ips = () => {
 };
 
 export const getDefaultHosts = () => {
-  return ["localhost", ...getLocalV4Ips()];
+  return ['localhost', ...getLocalV4Ips()];
 };
 
 /**
@@ -43,7 +43,7 @@ export const exec = async (cmd, options) => {
   return await promisify(cp_exec)(cmd, options);
 };
 
-export const DATA_DIR = join(homedir(), ".mkcert-cli");
+export const DATA_DIR = join(homedir(), '.mkcert-cli');
 /**
  * @param {string} path
  * @param {string=} baseDir
@@ -68,7 +68,7 @@ export const wrapInQuotes = (path) => {
 export const writeFile = async (filePath, data) => {
   if (!existsSync(filePath)) {
     await mkdir(dirname(filePath), { recursive: true });
-    console.log("Created directory: ", dirname(filePath));
+    console.log('Created directory: ', dirname(filePath));
   }
   await fsWriteFile(filePath, data);
   await chmod(filePath, 0o777);
@@ -79,7 +79,7 @@ export const writeFile = async (filePath, data) => {
  * @param {string} current
  */
 export const semverGreaterThan = (newest, current) =>
-  semver(newest.replace(/^v\.?/, ""), current.replace(/^v\.?/, "")) > 0;
+  semver(newest.replace(/^v\.?/, ''), current.replace(/^v\.?/, '')) > 0;
 
 /**
  * @param {string} filePath
@@ -90,10 +90,23 @@ export const ensureDirExist = async (filePath) => {
   }
 };
 
+/**
+ * Get absolute path from import.meta.url and relative descriptor
+ * @param {string} relative
+ * @param {string} filePath
+ * @returns {string}
+ */
+export const getAbsolutePath = (relative, filePath) => {
+  return new URL(relative, filePath).pathname;
+};
+
 export const pkgVersion = (() => {
+  const fileUrl = getAbsolutePath('../package.json', import.meta.url);
   try {
-    const pkgJson = (readFileSync('./package.json')).toString()
-    const { version } = JSON.parse( pkgJson || '');
+    const pkgJson = readFileSync(fileUrl).toString();
+    const { version } = JSON.parse(pkgJson || '');
     return `v${version}`;
-  } catch { /* om nom nom */ }
+  } catch {
+    /* om nom nom */
+  }
 })();
